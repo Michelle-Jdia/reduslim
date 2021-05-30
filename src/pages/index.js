5ebbcdf12efabe2e2618d2100a156740ce202304
Michelle Jdia <mj.sy.ru@gmail.com>
2021-05-30 14:12:51 +0300

    finish custom menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,30 @@
 import "./index.css";
 
+document
+  .querySelector(".header__custom-select-wrapper")
+  .addEventListener("click", function () {
+    this.querySelector(".header__custom-select").classList.toggle("open");
+  });
+
+for (const option of document.querySelectorAll(".header__custom-option")) {
+  option.addEventListener("click", function () {
+    if (!this.classList.contains("selected")) {
+      this.parentNode
+        .querySelector(".header__custom-option.selected")
+        .classList.remove("selected");
+      this.classList.add("selected");
+      this.closest(".header__custom-select").querySelector(
+        ".header__custom-select-trigger span"
+      ).textContent = this.textContent;
+      this.closest(".header__custom-select").querySelector(
+        ".header__custom-select-trigger span"
+      ).innerHTML = this.innerHTML;
+    }
+  });
+}
+window.addEventListener("click", function (e) {
+  const select = document.querySelector(".header__custom-select");
+  if (!select.contains(e.target)) {
+    select.classList.remove("open");
+  }
+});
