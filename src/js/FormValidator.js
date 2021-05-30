870ff0340808561372eff20573ba4302e8debc99
Michelle Jdia <mj.sy.ru@gmail.com>
2021-05-30 16:31:20 +0300

    added span start vakidat

---
+++ src/js/FormValidator.js
@@ -0,0 +1,8 @@
+const selectorList = {
+  formSelector: ".header__form",
+  inputSelector: ".header__input",
+  submitButtonSelector: ".header__btn",
+  inactiveButtonClass: "header__btn-submit_disabled",
+  inputErrorClass: "header__input-error",
+  errorClass: "header__input-error_visible",
+};
